09c0e967fc7e125e9cac14f394bb7d1164b0ced5
Thomas Fouquert <tfouquert@gmail.com>
2022-11-30 22:22:44 +0100

    update on tablerow component

--- src/components/TrackerRow.js
+++ src/components/TrackerRow.js
@@ -2,40 +2,39 @@ import React from 'react'
 import {diffTime} from '../helper'
 
 const TrackerRow = ({tracker, selectedTracker, onSelectedTracker}) => {
-  // 🐶 créé un variable 'starttime' qui contient un string au format 'toLocaleString'
-  // 🐶 créé un variable 'endtime' qui contient un string au format 'toLocaleString'
-  // 🐶 utilise une ternaire pour afficher la date de fin ou 'en cours ...'
-  // 📑 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-
-  // 🐶 nous allons maintenant vouloir refraichir automatiquement la durée toutes les secondes
-  // pour cela il faut changer 'duration' en state.
-  // ⛏️ Remplace duration en state
-  // const duration = diffTime(tracker?.starttime, tracker?.endtime)
-
-  // 🐶 L'idée et d'appeler une fonction 'refresh' toutes les secondes grace à 'setTimeOut'
-  // 📑 https://www.w3schools.com/jsref/met_win_settimeout.asp
-  // Utilise useEffect pour declarer la fonction 'refresh' et le 'timer'
-  React.useEffect(() => {
-    // 🐶 crée une fonction 'refresh' qui met à jour le state 'duration'
-    // 🐶 utilise setTimeout pour appler 'refresh' toutes les secondes
-    // 🤖 const timerID = setTimeout(() => refresh(), 1000)
-    // n'oulie pas le cleanup de useEffect en retournant 'clearTimeout(timerID)'
-    // 🐶 pense aux dependances
-  }, [])
   const handleClick = id => {
     onSelectedTracker(id)
   }
 
   const {id, name, starttime, endtime} = tracker
-  const duration = diffTime(starttime, endtime)
+
+  const selected = id === selectedTracker ? 'selectedline' : ''
+  const start = starttime.toLocaleString().split('T').join('  ')
+  const end = endtime
+    ? endtime.toLocaleString().split('T').join('  ')
+    : '...en cours'
+  const [duration, setDuration] = React.useState(diffTime(starttime, endtime))
+
+  React.useEffect(() => {
+    const refresh = () => {
+      setDuration(diffTime(starttime, endtime))
+    }
+
+    // 🐶 crée une fonction 'refresh' qui met à jour le state 'duration'
+    // 🐶 utilise setTimeout pour appler 'refresh' toutes les secondes
+    // 🤖
+    // n'oulie pas le cleanup de useEffect en retournant 'clearTimeout(timerID)'
+    // 🐶 pense aux dependances
+    const timerID = setTimeout(() => refresh(), 1000)
+
+    return () => clearTimeout(timerID)
+  }, [starttime, endtime])
+
   return (
-    <tr
-      onClick={() => handleClick(id)}
-      className={id === selectedTracker && 'selectedline'}
-    >
+    <tr onClick={() => handleClick(id)} className={selected}>
       <td>{name}</td>
-      <td>{starttime}</td>
-      <td>{endtime}</td>
+      <td>{start}</td>
+      <td>{end}</td>
       <td>{duration}</td>
     </tr>
   )
