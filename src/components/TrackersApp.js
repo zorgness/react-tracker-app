8e2ee20f6d9d171a7f16c2f06a33851249e047f4
Thomas Fouquert <tfouquert@gmail.com>
2022-11-30 15:47:56 +0100

    add filter tracker

--- src/components/TrackersApp.js
+++ src/components/TrackersApp.js
@@ -1,3 +1,28 @@
-// export * from './TrackersApp.final'
+/* eslint-disable no-unused-vars */
+import React, {Fragment} from 'react'
+import {FilterTrackers} from './FilterTrackers'
+import db from '../data'
 
-export * from './TrackersApp.exercise'
+function TrackersApp() {
+  const [allTrackers, setAllTrackers] = React.useState(db)
+  const [filterText, setFilterText] = React.useState('')
+  const [selectedTracker, setSelectedTracker] = React.useState({})
+
+  // 🐶 fonction qui va recuperer le texte saisie et filtrer notre base de données
+  const handleTextChange = text => {
+    setFilterText(text)
+    const filteredArray = db.filter(
+      track => track.name.toLowerCase().indexOf(text) !== -1,
+    )
+
+    setAllTrackers(filteredArray)
+  }
+
+  return (
+    <Fragment>
+      <FilterTrackers onTextChange={handleTextChange} />
+      <div>il y a {allTrackers.length} trackers</div>
+    </Fragment>
+  )
+}
+export {TrackersApp}
