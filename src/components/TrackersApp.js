1c6deb30ebff1c32b325915611be0ae4bfeabfbb
Thomas Fouquert <tfouquert@gmail.com>
2022-11-30 17:34:07 +0100

    add trackerstable

--- src/components/TrackersApp.js
+++ src/components/TrackersApp.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import React, {useState} from 'react'
-import db from '../data'
-
-function TrackersApp() {
-  const [allTrackers, setAllTrackers] = useState(db)
-
-  const [filterText, setFilterText] = useState('')
-
-  const [selectedTracker, setSelectedTracker] = useState({})
-
-  console.log(allTrackers)
-
-  return (
-    <div>
-      <h3>
-        il y a {allTrackers.length} tracker{allTrackers.length > 1 ? 's' : ''}
-      </h3>
-      {allTrackers.map(({id, name}) => {
-        return <p key={id}>{name}</p>
-      })}
-    </div>
-=======
-/* eslint-disable no-unused-vars */
 import React, {Fragment} from 'react'
 import {FilterTrackers} from './FilterTrackers'
 import db from '../data'
@@ -46,7 +22,6 @@ function TrackersApp() {
       <FilterTrackers onTextChange={handleTextChange} />
       <div>il y a {allTrackers.length} trackers</div>
     </Fragment>
->>>>>>> exercises/02-filtrer-tracker
   )
 }
 export {TrackersApp}
