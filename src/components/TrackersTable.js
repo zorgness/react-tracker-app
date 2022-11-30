91ea1eb4b6f05800ec72fd3f0ffa9b97dd54f817
Thomas Fouquert <tfouquert@gmail.com>
2022-11-30 18:30:54 +0100

    update

--- src/components/TrackersTable.js
+++ src/components/TrackersTable.js
@@ -1,12 +1,26 @@
 import * as React from 'react'
+import {groupBy} from '../helper'
 
-const TrackersTable = ({trackers}) => {
+const TrackerRow = ({tracker}) => {
   const findDuration = (starttime, endtime) => {
     if (!starttime || !endtime) return
     const duration = new Date(new Date(endtime) - new Date(starttime))
     return `${duration.getMinutes()}:${duration.getSeconds()}`
   }
 
+  const {name, starttime, endtime} = tracker
+
+  return (
+    <tr className="selectedline">
+      <td>{name}</td>
+      <td>{starttime}</td>
+      <td>{endtime}</td>
+      <td>{findDuration(starttime, endtime)}</td>
+    </tr>
+  )
+}
+
+const TrackersTable = ({trackers}) => {
   return (
     <div className="TableContainer">
       <h2>Liste des trackers</h2>
@@ -21,15 +35,8 @@ const TrackersTable = ({trackers}) => {
             </tr>
           </thead>
           <tbody>
-            {trackers.map(({id, name, starttime, endtime}) => {
-              return (
-                <tr key={id} className="selectedline">
-                  <td>{name}</td>
-                  <td>{starttime}</td>
-                  <td>{endtime}</td>
-                  <td>{findDuration(starttime, endtime)}</td>
-                </tr>
-              )
+            {trackers.map((tracker, {id}) => {
+              return <TrackerRow key={id} tracker={tracker} />
             })}
           </tbody>
         </table>
